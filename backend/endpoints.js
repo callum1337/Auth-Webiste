550260bcc6f21cfe59a797e72c9e1e1f04c2c5c1
Callum <76732321+callum1337@users.noreply.github.com>
2022-02-13 13:39:20 +0000

    sorted out syntax

--- backend/endpoints.js
+++ backend/endpoints.js
@@ -30,33 +30,45 @@ router.post('/users/login', (req, res, next) => {
 
 //POST OBJECTS
 router.post('/users/add', function(req, res) {
-        const user = req.body.user
-        const password = req.body.password
-        const password_hashed = hashPassword(password)
-        const email = req.body.email
-        connection.query('SELECT * FROM users WHERE username = ? OR email = ?', [user, email], function(err, result) {
-            if (err) {
-                console.log(err)
-                res.status(500).send('Internal Error')
-
-            }
+    const user = req.body.user
+    const password = req.body.password
+    if (password.length < 8) {
+        return res.status(400).send('Password must be at least 8 characters long')
+    }
+    if (user.length < 3) {
+        return res.status(400).send('Username must be at least 3 characters long')
+    }
+    const password_hashed = hashPassword(password)
+    const email = req.body.email
+    connection.query('SELECT * FROM users WHERE username = ? OR email = ?', [user, email], function(err, result) {
+        if (err) {
+            console.log(err)
+            return res.status(500).send('Internal Error')
+        } else{
             if (result.length > 0) {
                 res.status(409).send('User already exists')
             } else {
                 connection.query('INSERT INTO users (username, password, email) VALUES (?, ?, ?)', [user, password_hashed, email], function(err, result) {
                     if (err) {
                         console.log(err)
-                        res.status(500).send('Internal Error')
-
+                        return res.status(500).send('Internal Error')
+                    } else {
+                        res.status(201).send('User created')
                     }
-                    res.status(201).send('User created')
                 })
             }
-        })
-})
+        }})
+    });
 
 
 
+//add ip rate limit
+// router.post('/users/add', function(req, res) {
+//     const user = req.body.user
+//     const password = req.body.password
+//     //validate password
+//     if (password.length < 8) {
+        
 
 module.exports = router;
 
